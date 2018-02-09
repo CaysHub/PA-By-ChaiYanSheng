694ebce1c00754f8f3cec7a0d73b7c62ee33b588
tracer-ics2017 <tracer@njuics.org>
2018-02-09 21:43:26 +0800

    >  compile 161220000 cays Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:43:26 up  3:53,  2 users,  load average: 0.00, 0.00, 0.00 1ccfd45acf8e6eead7a37c78d9d746aabe355cf1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -77,7 +77,7 @@ static int cmd_x(char *args){
   printf("dump memory start address:0x%08x length:%d\n",address,length);
   for(i=0;i<length;i++){
     if(!(i&0xf))printf("\n0x%08x:",address+i*16);
-    printf("0x%02x ",*(unsigned char *)(address+i*16));
+   // printf("0x%02x ",*(unsigned char *)(address+i*16));
   }
   printf("\n");
   return 0;
