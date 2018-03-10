dd8f37d1b174319ad338aec0436a503a088dc679
161630108-ChaiYanSheng <1574411019@qq.com>
2018-03-10 15:24:51 +0800

    Add new_wp() and free_wp()

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -24,21 +24,66 @@ WP* new_wp(){
   WP *p=free_,*q,*s;
 	q=head;s=head->next;
 	if(p==NULL){
+		printf("Error:watchpoint struct have been all\n");
 	  assert(0);
-	}
-	if(p!=NULL){
+	}else if(p!=NULL){
 		if(head==NULL){
-		  
-		}
-	  while(s!=NULL&&q->NO<p->NO){
-		  q=q->next;
-			s=q->next;
+		  free_==free_->next;
+			head=p;
+			p->next=NULL;
+		}else if(head!=NULL){
+			if(q->NO>p->NO){
+			  free_=p->next;
+				p->next=head;
+				head=p;
+			}else{
+		    while(s!=NULL&&q->NO<p->NO){
+			    q=q->next;
+				  s=q->next;
+			  }
+			  q->next=p;
+			  free_=p->next;
+			  p->next=s;
+			}
 		}
 	}
 	return p;
 }
 
 void free_wp(WP* wp){
+	WP *p,*q,*s,*r;
+	p=head;q=p->next;
+	r=free_;s=r->next;
+	if(head==NULL){
+	  printf("Error:No such watchpoint\n");
+		assert(0);
+	}else if(head!=NULL){
+	  if(head==wp){
+			q=head;
+		  head=head->next;
+		}else{
+		  while(q!=NULL&&q!=wp){
+			  p=p->next;
+				q=p->next;
+			}
+			if(q==NULL){
+			  printf("Error:No such watchoint\n");
+				assert(0);
+			}
+			p->next=q->next;
+		}
+		if(free_==NULL||q->NO<free_->NO){
+		  q->next=free_;
+			free_=q;
+		}else{
+		  while(r!=NULL&&s->NO<q->NO){
+			  s=s->next;
+				r=s->next;
+			}
+			s->next=q;
+			q->next=r;
+		}
+	}
   return;
 }
 
