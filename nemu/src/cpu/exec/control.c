bc26108332edb68ea18643d7be839dfc87daaafc
tracer-ics2017 <tracer@njuics.org>
2018-04-06 15:57:44 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:57:44 up  4:19,  3 users,  load average: 0.00, 0.00, 0.00 4c12657a4966099e7fdbefb4b1dc77988b515b28

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -30,7 +30,7 @@ make_EHelper(call) {
 	}else{
 		printf("0x%x\n",cpu.eip);
 	  rtl_push(&cpu.eip);
-		cpu.eip+=decoding.src.val;
+		cpu.eip+=id_dest->val;
 		printf("0x%x\n",cpu.eip);
 	}
   decoding.is_jmp=1;
