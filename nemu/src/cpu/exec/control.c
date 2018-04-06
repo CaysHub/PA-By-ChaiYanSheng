e394c02f9e28b3df6714ac412fab44eed4cf245a
161630108-ChaiYanSheng <1574411019@qq.com>
2018-04-06 15:07:53 +0800

    Add call rel32 instruction

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,8 +25,14 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
-
+  // TODO();
+  if(decoding.is_operand_size_16){
+	}else{
+	  rtl_push(&cpu.eip);
+		cpu.eip+=id_src->val;
+	}
+  decoding.is_jmp=1;
+	decoding.jmp_eip=cpu.eip;
   print_asm("call %x", decoding.jmp_eip);
 }
 
