21e141891dcea092913f5bb35899e78f6b901a68
tracer-ics2017 <tracer@njuics.org>
2018-04-09 22:58:09 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:58:09 up  1:52,  3 users,  load average: 0.00, 0.01, 0.00 3b651d8376836b5edc453c9798909fae2fb81196

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -42,7 +42,7 @@ static inline make_DopHelper(SI) {
   // TODO();
 	op->simm=instr_fetch(eip,op->width);
 	printf("SI:op->width:%d\n",op->width);
-	printf("Opcode:0x%x\nSrc:0x%x\n",decoding.opcode,decoding.src.val);
+	printf("Opcode:0x%x\nSrc:0x%x\nOp:0x%x\n",decoding.opcode,decoding.src.val,op->simm);
 	op->val=op->simm;
   rtl_li(&op->val, op->simm);
 
