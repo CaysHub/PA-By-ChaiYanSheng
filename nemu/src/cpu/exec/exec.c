8f4bc1725f4e50339a97eac694e8c5e6af113d58
tracer-ics2017 <tracer@njuics.org>
2018-04-11 14:50:55 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:50:55 up 10 min,  3 users,  load average: 0.00, 0.00, 0.00 af98781f5eae9a4b8e46a29633f5fac0dc3d28a3

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -23,7 +23,7 @@ static inline void set_width(int width) {
 /* Instruction Decode and EXecute */
 static inline void idex(vaddr_t *eip, opcode_entry *e) {
   /* eip is pointing to the byte next to opcode */
-  if (e->decode)
+  if (e->execute)
 	{e->decode(eip);
   e->execute(eip);}
 }
