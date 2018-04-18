d89d94eff8b8bfd4b76661ce83a29a24c5a73063
tracer-ics2017 <tracer@njuics.org>
2018-04-18 23:21:07 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:21:07 up  3:00,  4 users,  load average: 0.00, 0.00, 0.00 5f613633c9a704065ce86b49b344c16134bad8e9

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -9,7 +9,7 @@ make_EHelper(test) {
 make_EHelper(and) {
 	rtl_sext(&id_src->val,&id_src->val,id_src->width);
 	id_src->width=id_dest->width;
-	printf("src:0x%x\n",id_src->val);
+	printf("src:0x%x\ndest:0x%x\n",id_src->val,id_dest->val);
   rtl_and(&t0,&id_dest->val,&id_src->val);
 	operand_write(id_dest,&t0);
 	printf("dest:0x%x\n",id_dest->val);
