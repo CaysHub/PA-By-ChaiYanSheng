6c4c7e91a2a6c86bfc862e8358eb886665612669
tracer-ics2017 <tracer@njuics.org>
2018-04-20 20:52:20 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:52:20 up  2:22,  4 users,  load average: 0.02, 0.02, 0.00 fca77a1a2901031c300c4fc6551fe0aaaf2990fb

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -229,10 +229,10 @@ void exec_wrapper(bool print_flag) {
   decoding.p = decoding.asm_buf;
   decoding.p += sprintf(decoding.p, "%8x:   ", cpu.eip);
 #endif
-
+  printf("esp:0x%x\nebp:0x%x\n",cpu.esp,cpu.ebp);
   decoding.seq_eip = cpu.eip;
   exec_real(&decoding.seq_eip);
-	printf("esp:0x%x\nebp:0x%x\n",cpu.esp,cpu.ebp);
+
 #ifdef DEBUG
   int instr_len = decoding.seq_eip - cpu.eip;
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
