cfbac306566c7fe203f02c2ec8abcd7031828617
tracer-ics2017 <tracer@njuics.org>
2018-04-20 21:07:02 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:07:02 up  2:37,  4 users,  load average: 0.09, 0.03, 0.01 b91352a82ee479241e94cb4e0f9251b73723ea1

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -234,15 +234,13 @@ void exec_wrapper(bool print_flag) {
   exec_real(&decoding.seq_eip);
 
 #ifdef DEBUG
-  int instr_len = decoding.seq_eip - cpu.eip;
-	printf("esp:0x%x\nebp:0x%x\n",cpu.esp,cpu.ebp);
+  //int instr_len = decoding.seq_eip - cpu.eip;
 
-  sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
+	printf("esp:0x%x\nebp:0x%x\n",cpu.esp,cpu.ebp);
+  //sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
 	printf("esp:0x%x\nebp:0x%x\n",cpu.esp,cpu.ebp);
 
   strcat(decoding.asm_buf, decoding.assembly);
-	printf("esp:0x%x\nebp:0x%x\n",cpu.esp,cpu.ebp);
-
   Log_write("%s\n", decoding.asm_buf);
   if (print_flag) {
     puts(decoding.asm_buf);
