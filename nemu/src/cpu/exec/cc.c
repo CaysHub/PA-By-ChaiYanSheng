f5abe0adaa50ea22a0229e02ad14c50bec427b5a
tracer-ics2017 <tracer@njuics.org>
2018-04-21 18:23:28 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  18:23:28 up  5:31,  4 users,  load average: 0.00, 0.00, 0.00 a4e8fb7dc1098d8c5e6c798dd89a8c17729f6b46

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,15 +14,14 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
+    case CC_O:invert=(cpu.eflags.OF==1);break;
+    case CC_B:invert=(cpu.eflags.CF==1);break;
+    case CC_E:invert=(cpu.eflags.ZF==1);break;
+    case CC_BE:invert=(cpu.eflags.CF==1||cpu.eflags.ZF==1);break;
+    case CC_S:invert=(cpu.eflags.SF==1);break;
+    case CC_L:invert=(cpu.eflags.SF!=cpu.eflags.OF);break;
+    case CC_LE:invert=(cpu.eflags.ZF==1 || cpu.eflags.SF!=cpu.eflags.OF);break;
       //TODO();
-			break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
