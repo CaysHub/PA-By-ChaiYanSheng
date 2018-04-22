f3fe8ce71f6280d27897d599b3fd9118a7c95e91
tracer-ics2017 <tracer@njuics.org>
2018-04-22 10:48:15 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:48:15 up  1:15,  4 users,  load average: 0.10, 0.04, 0.01 810cf9a1d402a5a1a32c16f8997c41c23464079f

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -3,7 +3,7 @@
 make_EHelper(jmp) {
   // the target address is calculated at the decode stage
   decoding.is_jmp = 1;
-  printf("jmp_eip:0x%x\ndest+eip:0x%x\n",decoding.jmp_eip,id_dest->simm+*eip);
+  printf("jmp_eip:0x%x\nsimm:0x%x\ndest+1:0x%x\n",decoding.jmp_eip,id_dest->simm,id_dest->simm+1);
   print_asm("jmp %x", decoding.jmp_eip);
 }
 
