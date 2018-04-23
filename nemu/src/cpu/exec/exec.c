cbea9defe06253f263b3c55c43497a1cfb288b1c
tracer-ics2017 <tracer@njuics.org>
2018-04-23 12:57:21 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  12:57:21 up 32 min,  4 users,  load average: 0.03, 0.01, 0.00 92abf1a5ace3272c55416dc0206e950b4617be6b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -104,7 +104,7 @@ opcode_entry opcode_table [512] = {
   /* 0x70 */	IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1),
   /* 0x74 */	IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1),
   /* 0x78 */	IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1),
-  /* 0x7c */	IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(I,jcc,1),
+  /* 0x7c */	IDEXW(I,jcc,1), IDEXW(I,jcc,1), IDEXW(SI,jcc,1), IDEXW(I,jcc,1),
   /* 0x80 */	IDEXW(I2E, gp1, 1), IDEX(I2E, gp1), EMPTY, IDEX(SI2E,gp1),//IDEX(SI2E, gp1),
   /* 0x84 */ IDEXW(G2E,test,1), IDEXW(G2E,test,0), IDEXW(G2E,nop,1), IDEX(G2E,nop),
   /* 0x88 */	IDEXW(mov_G2E, mov, 1), IDEX(mov_G2E, mov), IDEXW(mov_E2G, mov, 1), IDEX(mov_E2G, mov),
