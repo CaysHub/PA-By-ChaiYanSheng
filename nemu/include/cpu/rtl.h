429724f30a9f2ad9101492f825fe709967e7fee1
tracer-ics2017 <tracer@njuics.org>
2018-04-23 15:33:10 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:33:10 up 39 min,  4 users,  load average: 0.01, 0.02, 0.00 8d85109ad684fcaeb885acc39660f4adec0f8f3a

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -148,6 +148,9 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
 	int a=*src1;
   int b=a<<(32-(width<<3));int c=b>>(32-(width<<3));
+	printf("sext:src:0x%x\n",*src1);
+  printf("sext:b:0x%x\n",b);
+	printf("sext:c:0x%x\n",c);
 	*dest=c;
 }
 
