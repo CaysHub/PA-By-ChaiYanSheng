5ef2a978a092401fe815f6716a6fe9fec66d8c2b
tracer-ics2017 <tracer@njuics.org>
2018-04-23 15:31:24 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:31:24 up 37 min,  4 users,  load average: 0.05, 0.03, 0.00 394c5026cf213fd25c377f95955217793a1257ab

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -45,7 +45,7 @@ make_EHelper(cmp) {
   t0=id_dest->val;
 	t3=id_src->val;
 	rtl_sext(&t1,&t3,id_dest->width);
-	printf("cmp:t1:0x%x\ndest_width:0x%x\n",t1,id_dest->width);
+	//printf("cmp:t1:0x%x\ndest_width:0x%x\n",t1,id_dest->width);
 	rtl_sub(&t2,&t0,&t1);
 
 	rtl_update_ZFSF(&t2,id_dest->width);
