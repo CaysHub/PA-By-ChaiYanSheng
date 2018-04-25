d47c25fa54a6b563bfbddc0b12f2bc5f3575c417
tracer-ics2017 <tracer@njuics.org>
2018-04-25 14:25:18 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:25:18 up 12 min,  4 users,  load average: 0.08, 0.07, 0.02 380916105f0a30cd3263c6b8d8c2c5b44bcb5f4a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -24,7 +24,6 @@ make_EHelper(sub) {
 	rtl_sub(&t2,&id_dest->val,&id_src->val);
 	t1=id_dest->val;
 	operand_write(id_dest,&t2);
-  printf("sub:dest:0x%x\tsrc:0x%x\n",id_dest->val,id_src->val);
 	rtl_update_ZFSF(&t2,id_dest->width);
 
 	t2=~id_src->val;
@@ -38,7 +37,6 @@ make_EHelper(sub) {
 		if(i==id_dest->width*8-1)an_1=an;
 	}
   t3=(!an)?1:0;rtl_set_CF(&t3);
-	printf("sub:CF:0x%x\n",t3);
 	t3=an^an_1;rtl_set_OF(&t3);
   print_asm_template2(sub);
 }
@@ -173,7 +171,7 @@ make_EHelper(sbb) {
   rtl_sltu(&t3, &id_dest->val, &t2);
   rtl_get_CF(&t1);
   rtl_sub(&t2, &t2, &t1);
-	printf("sbb:dest:0x%x\tsrc:0x%x\tCF:0x%x\n",id_dest->val,id_src->val,t1);
+	
   operand_write(id_dest, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
