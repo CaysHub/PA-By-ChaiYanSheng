4987cb4ac410a09c06168629aeb08098fdcf0d39
tracer-ics2017 <tracer@njuics.org>
2018-04-25 11:36:26 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:36:26 up 35 min,  4 users,  load average: 0.05, 0.01, 0.01 ea0241edf77aa3a24bb5ad88f3d902e5bc99a8f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -172,6 +172,7 @@ make_EHelper(sbb) {
   rtl_sltu(&t3, &id_dest->val, &t2);
   rtl_get_CF(&t1);
   rtl_sub(&t2, &t2, &t1);
+	printf("sbb:dest:0x%x\tsrc:0x%x\tCF:0x%x\n",id_dest->val,id_src->val,t1);
   operand_write(id_dest, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
