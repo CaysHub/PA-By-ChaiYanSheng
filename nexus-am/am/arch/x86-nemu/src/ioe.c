a8c16befdd0daf831516e02ea4f861f4e01690d8
tracer-ics2017 <tracer@njuics.org>
2018-04-29 01:27:34 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  01:27:34 up  2:33,  3 users,  load average: 0.28, 0.13, 0.05 c40c5d18b78f641762dab28ef37757bbadcf794b

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,9 +9,6 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-	//struct timeval now;
-  //_gettimeofday(&now, NULL);
-	//unsigned long seconds = now.tv_sec;
 	unsigned long current_seconds=inl(0);
   return current_seconds-boot_time;
 }
