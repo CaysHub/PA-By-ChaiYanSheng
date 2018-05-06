9a4bcaf52f9ece5226040b1285c4bc7969259455
tracer-ics2017 <tracer@njuics.org>
2018-05-06 11:48:11 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:48:11 up  8:14,  1 user,  load average: 1.23, 0.42, 0.15 e6efb0cc3bc82bde7ea09c2252a691ce5d2901b4

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -69,6 +69,6 @@ make_EHelper(call_rm) {
 	}
 	decoding.is_jmp=1;
   //printf("call_rm:opcode:0x%x\tdest:0x%x\n",decoding.opcode,id_dest->val);
-	printf("call_rm:jmp_eip:0x%x\n",decoding.jmp_eip);
+	//printf("call_rm:jmp_eip:0x%x\n",decoding.jmp_eip);
   print_asm("call *%s", id_dest->str);
 }
