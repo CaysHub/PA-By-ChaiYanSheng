b257f47a61e05434e3272b095f4e08af8cb84286
tracer-ics2017 <tracer@njuics.org>
2018-05-16 21:49:58 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:49:58 up 54 min,  4 users,  load average: 0.01, 0.07, 0.20 aa48470efbdea391ed9e8605d63e1338b3916c0

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,7 +5,7 @@ void ramdisk_read(void *buf, off_t offset, size_t len);
 void ramdisk_write(const void *buf, off_t offset, size_t len);
 size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
 	size_t len=get_ramdisk_size();
 	ramdisk_write(DEFAULT_ENTRY,0,len);
 	return (uintptr_t)DEFAULT_ENTRY;
