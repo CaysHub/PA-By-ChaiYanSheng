1668ae55b0164f4c7709118317e98cbb554ff70f
tracer-ics2017 <tracer@njuics.org>
2018-05-17 22:58:17 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:58:17 up  2:16,  4 users,  load average: 0.13, 0.09, 0.02 fc7bc6f3d463a1c31c61fe094fd122385a915a95

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -15,7 +15,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 	a.offset_15_0=vaddr_read(cpu.IDTR.base+NO*8,2);
 	a.offset_31_16=vaddr_read(cpu.IDTR.base+NO*8+6,2);
 	a.present=(vaddr_read(cpu.IDTR.base+NO*8+4,2)>>15)&0x1;
-	assert(a.present==0);
+	assert(a.present==1);
 	uint32_t a1=a.offset_15_0,a2=a.offset_31_16;
 	decoding.jmp_eip=(a2<<16)+a1;
 	decoding.is_jmp=1;
