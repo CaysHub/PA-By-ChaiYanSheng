7c772dd7337ac7597434308233337f75f9367e1d
tracer-ics2017 <tracer@njuics.org>
2018-05-17 22:56:41 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:56:41 up  2:14,  4 users,  load average: 0.07, 0.04, 0.00 383665169b3b392bc85268e2fa96d62811a59b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,9 +11,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 	uint32_t cs=cpu.CS.val;
 	rtl_push(&cs);
 	rtl_push(&ret_addr);
-	GateDesc a;a.val=vaddr_read(cpu.IDTR.base+NO*8,8);
-	/*a.offset_15_0=vaddr_read(cpu.IDTR.base+NO*8,2);
-	a.offset_31_16=vaddr_read(cpu.IDTR.base+NO*8+6,2);*/
+	GateDesc a;
+	a.offset_15_0=vaddr_read(cpu.IDTR.base+NO*8,2);
+	a.offset_31_16=vaddr_read(cpu.IDTR.base+NO*8+6,2);
+	a.present=(vaddr_read(cpu.IDTR.base+NO*8+4,2)>>15)&0x1;
 	printf("p:0x%x\n",a.present);
 	uint32_t a1=a.offset_15_0,a2=a.offset_31_16;
 	decoding.jmp_eip=(a2<<16)+a1;
