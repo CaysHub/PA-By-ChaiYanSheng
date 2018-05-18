b3f37692739f5b5b7961e90867241287a44a8cea
tracer-ics2017 <tracer@njuics.org>
2018-05-18 17:58:53 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  17:58:53 up  1:21,  4 users,  load average: 0.10, 0.04, 0.01 14f7533a4470c6ffa4d2390da3fc44f3b3d4a217

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -8,7 +8,7 @@
 
 struct _RegSet {
   //uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
-  uintptr_t edi,esi,ebp,esp,ebx,edx,ecx,eax;
+  //uintptr_t edi,esi,ebp,esp,ebx,edx,ecx,eax;
 	uintptr_t error_code;
 	int       irq;
 	//uintptr_t eax,ecx,edx,ebx,esp,ebp,esi,edi;
