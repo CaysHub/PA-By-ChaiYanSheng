b46d2f6b7c5f475aa30173714b24ed0b2278a5b6
tracer-ics2017 <tracer@njuics.org>
2018-05-18 12:43:05 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  12:43:05 up  1:00,  4 users,  load average: 0.00, 0.00, 0.00 327e1fec81f00e25e60a928ab50aceacfb7f0a2c

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,8 +7,10 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t error_code,eflags,cs,eip,eax,ecx,edx,ebx,esp,ebp,esi,edi;
-  int       irq;
+  //uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+  uintptr_t edi,esi,ebp,esp,ebx,edx,ecx,eax;
+	int       irq;
+	uintptr_t error_code,eip,cd,eflags;
 };
 
 #define SYSCALL_ARG1(r) 0
