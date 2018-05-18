35bf02dd3684be54c47432ae2ba699162676a7ab
tracer-ics2017 <tracer@njuics.org>
2018-05-18 23:39:29 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:39:29 up  7:01,  4 users,  load average: 0.05, 0.13, 0.06 7b7a1bc996aa442bd41d515c7a826c138e4dbd90

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -39,13 +39,13 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
+  //TODO();
   if(decoding.is_operand_size_16){
 	  assert(0);
 	}else{
 	  rtl_pop(eip);
 		rtl_pop(&t0);
-		cpu.CS.val=t0;
+		cpu.CS.val=t0&0xffff;
 		rtl_pop(&cpu.eflags.val);
 	}
   print_asm("iret");
