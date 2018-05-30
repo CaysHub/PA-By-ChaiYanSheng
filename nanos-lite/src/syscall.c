6f24e7aec192a185eb93976f0b68d4cc8aa85b65
tracer-ics2017 <tracer@njuics.org>
2018-05-30 22:13:38 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:13:38 up  2:40,  3 users,  load average: 0.09, 0.03, 0.01 a52f0c5ba6884817cf87879f1548b9cf98f24949

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,7 @@ _RegSet* do_syscall(_RegSet *r) {
 
   switch (a[0]) {
 		case SYS_none:SYSCALL_ARG1(r)=sys_none();break;
-		case SYS_exit:sys_exit(0);break;
+		//case SYS_exit:sys_exit(0);break;
 		//case SYS_write:SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
