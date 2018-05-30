2c6b6ac08376f766481fb56eb56517d821c4b66d
tracer-ics2017 <tracer@njuics.org>
2018-05-30 09:32:15 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  09:32:15 up 32 min,  3 users,  load average: 0.08, 0.03, 0.05 b8f55f26febb343cfb51a52882ed693db70ac7d6

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,6 +2,7 @@
 #include "syscall.h"
 int sys_none();
 void sys_exit(int code);
+int sys_write(int fd,void *buf,int len);
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -9,6 +10,8 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
 		case SYS_none:SYSCALL_ARG1(r)=sys_none();break;
 		case SYS_exit:sys_exit(0);break;
+		case SYS_write:SYSCALL_ARG1(r)=sys_write(SYSCALL_ARG2(r),(void*)
+					SYSCALL_ARG3(r),SYSCALL_ARG4(r));break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
@@ -20,4 +23,13 @@ int sys_none(){
 }
 void sys_exit(int code){
   _halt(code);
+}
+int sys_write(int fd,void *buf,int len){
+  if(fd==1||fd==2){
+	  int i=0;
+		for(i=0;i<len;i++){
+		  _putc(((char*)buf)[i]);
+		}
+	}
+	return len;
 }
