b6028c0e322a405370ca5845efbb89ea6f031378
tracer-ics2017 <tracer@njuics.org>
2018-05-31 20:05:25 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:05:25 up 40 min,  3 users,  load average: 0.02, 0.17, 0.14 f0cddcf9d00447b23d2a1861bbd9af8c78d98974

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,7 @@
 int sys_none();
 void sys_exit(int code);
 int sys_write(int fd,void *buf,int len);
-int sys_brk();
+int sys_brk(void *addr);
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -15,7 +15,7 @@ _RegSet* do_syscall(_RegSet *r) {
 		case SYS_none:SYSCALL_ARG1(r)=sys_none();break;
 		case SYS_exit:sys_exit(0);break;
 		case SYS_write:SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);break;
-		case SYS_brk:SYSCALL_ARG1(r)=sys_brk();break;
+		case SYS_brk:SYSCALL_ARG1(r)=sys_brk((void*)a[2]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
@@ -40,6 +40,6 @@ int sys_write(int fd,void *buf,int len){
 	}
 	return len;
 }
-int sys_brk(){
+int sys_brk(void *addr){
   return 0;
 }
