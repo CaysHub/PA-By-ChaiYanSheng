d84fc849371d09cb578cf6ac4d7deac9634f1bdc
tracer-ics2017 <tracer@njuics.org>
2018-06-04 23:48:18 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:48:18 up 11:05,  3 users,  load average: 0.22, 0.16, 0.06 ca2c082b97ad17adb4a79694e5962513fcb40445

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,8 +19,8 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-	int i=offset/_screen.height,j=offset%_screen.height;
-	_draw_rect(buf,i,j,len,1);
+	int i=offset/_screen.height,j=offset%_screen.height-1;
+	_draw_rect(buf,i,j,len,len);
 
 }
 
