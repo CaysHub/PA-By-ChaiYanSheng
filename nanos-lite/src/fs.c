45fa30874ebc3e9665174c842f404a4f6097dbdb
tracer-ics2017 <tracer@njuics.org>
2018-06-05 22:11:59 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:11:59 up  3:07,  3 users,  load average: 0.11, 0.18, 0.33 475b191f2ef547414438964d6ae2a14d46443cc9

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -48,15 +48,13 @@ ssize_t fs_read(int fd, void *buf, size_t len){
 	if(fd<3){
 	  panic("fs_resd fd<3");
 	}
-	if(fd==FD_EVENTS){
-	  size_t len2=events_read(buf,len);
-		return len2>len?len:len2;
-	}
 	if(file_table[fd].open_offset+len>file_table[fd].size){
 	  len=file_table[fd].size-file_table[fd].open_offset;
 	}
 	Log("fs_read fd: %d,len: %d",fd,len);
-	if(fd==FD_DISPINFO){
+	if(fd==FD_EVENTS){
+	  events_read(buf,len);
+	}else if(fd==FD_DISPINFO){
 		dispinfo_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
 	}else{
 	  ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
