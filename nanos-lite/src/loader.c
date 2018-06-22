971301ae8c9643f45e9cb89dcc96daf6d985cbbc
tracer-ics2017 <tracer@njuics.org>
2018-06-22 20:53:36 +0800

    >  run 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:53:36 up  3:07,  2 users,  load average: 0.12, 0.04, 0.01 5e7304a993a5372b7e26cefad0c9091893cab3db

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include "common.h"
-
-#define DEFAULT_ENTRY ((void *)0x4000000)
+#define PAGE_SIZE 4096
+#define DEFAULT_ENTRY ((void *)0x8048000)
 void ramdisk_read(void *buf, off_t offset, size_t len);
 void ramdisk_write(const void *buf, off_t offset, size_t len);
 size_t get_ramdisk_size();
@@ -10,12 +10,21 @@ ssize_t fs_write(int fd, const void *buf, size_t len);
 off_t fs_lseek(int fd, off_t offset, int whence);
 int fs_close(int fd);
 size_t fs_filesz(int fd);
+void *new_page();
+
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
 	//size_t len=get_ramdisk_size();
 	//ramdisk_read(DEFAULT_ENTRY,0,len);
 	int fd=fs_open(filename,0,0);
-	fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
-	assert(fs_close(fd)==0);
+	size_t fsz=fs_filesz(fd);
+	void *pa,*va;
+	for(va=DEFAULT_ENTRY;va<DEFAULT_ENTRY+fsz;va+=PAGE_SIZE){
+		pa=new_page();
+	  Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+	  _map(as, va, pa);
+	}
+	//fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+	//assert(fs_close(fd)==0);
 	return (uintptr_t)DEFAULT_ENTRY;
 }
