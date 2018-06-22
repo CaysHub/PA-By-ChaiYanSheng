71940007246a01175e646f59eb679fdb3fd7659b
tracer-ics2017 <tracer@njuics.org>
2018-06-22 21:24:06 +0800

    >  compile 161630108 chaiyansheng Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:24:06 up  3:38,  2 users,  load average: 0.00, 0.00, 0.00 2f480e6262f9f56669f8fed38dbeaae447fe8989

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,9 +33,9 @@ int main() {
 
   //uint32_t entry = loader(NULL, "/bin/pal");
   //((void (*)(void))entry)();
-	Log("2");
+	//Log("2");
 	load_prog("/bin/dummy");
-	Log("3");
+	//Log("3");
 
   panic("Should not reach here");
 }
